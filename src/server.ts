17b236cf9d13a1df246291a57086772c6ee8123f
artego <artego974@gmail.com>
2025-10-14 14:07:15 -0300

    alterando links consumo de API pelo vercel

--- src/server.ts
+++ src/server.ts
@@ -19,7 +19,7 @@ app.use(
       "http://localhost:3000",
       "http://127.0.0.1:3000",
       "http://127.0.0.1:5500",
-      "https://educa-tea-eight.vercel.app/"
+      "https://educa-tea.vercel.app/"
     ],
   })
 );
